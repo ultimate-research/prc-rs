4b697701a512c342a767183364423e4c3a189d7e
BenHall-7 <benjaminjahall@gmail.com>
2019-08-06 15:37:23 -0400

    Param enum outline

---
+++ src/param/param.rs
@@ -0,0 +1,16 @@
+use std::collections;
+
+pub enum ParamKind { //index starts at 1
+    Bool(bool),
+    I8(i8),
+    U8(u8),
+    I16(i16),
+    U16(u16),
+    I32(i32),
+    U32(u32),
+    Float(f32),
+    Hash(u64),
+    Str(String),
+    List(Vec<ParamKind>),
+    Struct(Vec<(u64, ParamKind)>)
+}
