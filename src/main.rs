bca63aa4d352c0388f50a5da8d317ff5fed209fb
BenHall-7 <benjaminjahall@gmail.com>
2019-08-10 19:28:28 -0400

    Write output to file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use prc;
 use serde_json::to_string_pretty;
 use std::time::Instant;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
     let now = Instant::now();
+    println!("Initializing file...");
     match prc::open("Path\\To\\Param\\File.prc") {
         Ok(x) => {
-            println!("OK! Serializing to json...");
-            println!("{}", to_string_pretty(&x).unwrap())
+            println!("Serializing to json...");
+            let mut file = File::create("out.json").unwrap();
+            file.write_all(to_string_pretty(&x).unwrap().as_bytes()).unwrap();
+            println!("Done!")
         }
         Err(x) => println!("{}", x),
     }
-    println!("elapsed time: {}", now.elapsed().as_millis());
+    println!("elapsed milliseconds: {}", now.elapsed().as_millis());
 }
