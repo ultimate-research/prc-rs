1ae1c4464d8fb00bebe69f801ed03b8b367cf51f
BenHall-7 <benjaminjahall@gmail.com>
2019-11-03 17:12:54 -0500

    empty main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
-use prc;
-use serde_yaml::to_string;
-use std::fs::File;
-use std::io::Write;
-use std::time::Instant;
-
 fn main() {
-    let now = Instant::now();
-    println!("Initializing file...");
-    match prc::open(
-        r"C:\Users\Breakfast\Documents\_Ultimate\root\param\spirits\campaign\0xF9FDA894.prc",
-    ) {
-        Ok(x) => {
-            println!("Serializing...");
-            let mut file = File::create("out.yml").unwrap();
-            file.write_all(to_string(&x).unwrap().as_bytes())
-                .unwrap();
-            println!("Done!")
-        }
-        Err(x) => println!("{}", x),
-    }
-    println!("elapsed milliseconds: {}", now.elapsed().as_millis());
+    
 }
