df9e9cd17764830e278e9f713dbc60a0d01c34c5
BenHall-7 <benjaminjahall@gmail.com>
2020-04-15 03:42:54 -0400

    Add a testable main

--- src/param-xml/main.rs
+++ src/param-xml/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use prc::open;
+use prc::xml::to_xml;
+
+use std::fs::File;
+use std::io::BufWriter;
+use std::time::Instant;
+
+fn main() {
+    let mut now = Instant::now();
+    let filename = std::env::args().nth(1).unwrap();
+    let param = open(&filename).unwrap();
+    println!("Opened in {}", now.elapsed().as_secs_f32());
+
+    now = Instant::now();
+    let mut writer = BufWriter::new(File::create("output.xml").unwrap());
+    to_xml(&param, &mut writer).unwrap();
+    println!("Converted to XML in {}", now.elapsed().as_secs_f32());
+}
